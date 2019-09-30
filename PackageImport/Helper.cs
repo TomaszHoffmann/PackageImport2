aeb7e4e891f7c81c84df38bdbebb8c0d727119d3
Tomasz Hoffmann <tomaszhoffmann0@gmail.com>
2019-09-30 08:42:36 +0200

    Saving drive rod's parameters

--- PackageImport/Helper.cs
+++ PackageImport/Helper.cs
@@ -25,6 +25,47 @@ namespace PackageImport
             public int ok_cel { get; set; }
         }
 
+        public struct ParametersOfDriveRods
+        {
+            public string nr_art { get; set; }
+
+            public int handleHeight { get; set; }
+            public int dornmass { get; set; }
+
+            public int heightOfDriveRodBox1 { get; set; }
+            public int depthOfDriveRodBox1 { get; set; }
+            public int thicknessOfDriveRodBox1 { get; set; }
+            public int shiftOfDriveRodBox1 { get; set; }
+
+            public int heightOfDriveRodBox2 { get; set; }
+            public int depthOfDriveRodBox2 { get; set; }
+            public int thicknessOfDriveRodBox2 { get; set; }
+            public int shiftOfDriveRodBox2 { get; set; }
+
+            public int heightOfDriveRodBox3 { get; set; }
+            public int depthOfDriveRodBox3 { get; set; }
+            public int thicknessOfDriveRodBox3 { get; set; }
+            public int shiftOfDriveRodBox3 { get; set; }
+
+            public int heightOfDriveRodBox4 { get; set; }
+            public int depthOfDriveRodBox4 { get; set; }
+            public int thicknessOfDriveRodBox4 { get; set; }
+            public int shiftOfDriveRodBox4 { get; set; }
+
+            public int heightOfDriveRodBox5 { get; set; }
+            public int depthOfDriveRodBox5 { get; set; }
+            public int thicknessOfDriveRodBox5 { get; set; }
+            public int shiftOfDriveRodBox5 { get; set; }
+
+            public int heightOfDriveRodBox6 { get; set; }
+            public int depthOfDriveRodBox6 { get; set; }
+            public int thicknessOfDriveRodBox6 { get; set; }
+            public int shiftOfDriveRodBox6 { get; set; }
+
+
+
+        }
+
         ResourceManager resManager = Resources.ProgramResourcesPL.ResourceManager;
 
         public DataTable GetTable(DataSet dataset, string name)
--- PackageImport/Program.cs
+++ PackageImport/Program.cs
@@ -26,6 +26,7 @@ namespace PackageImport
         IHelper helper = new Helper();
 
         List<Helper.ArticlesNhandloInfo> articlesNhandlo = new List<Helper.ArticlesNhandloInfo>();
+        List<Helper.ParametersOfDriveRods> parametersOfDriveRods = new List<Helper.ParametersOfDriveRods>();
 
         ResourceManager resManager = Resources.ProgramResourcesPL.ResourceManager ;
        
@@ -71,6 +72,47 @@ namespace PackageImport
                     };
 
                     articlesNhandlo.Add(ArticlesInfo);
+
+                    Helper.ParametersOfDriveRods parametersOfDriveRodsInfo = new Helper.ParametersOfDriveRods()
+                    {
+                        nr_art = Convert.ToString(row["Materiał"]),
+
+                        handleHeight = Convert.ToInt32(row["Wysokość Klamki"]),
+                        dornmass = Convert.ToInt32(row["Dornmass"]),
+
+                        heightOfDriveRodBox1 = Convert.ToInt32(row["Puszka Zasuw. 1 Wysokość"]),
+                        depthOfDriveRodBox1 = Convert.ToInt32(row["Puszka Zasuw. 1 Głębokość"]),
+                        thicknessOfDriveRodBox1 = Convert.ToInt32(row["Puszka Zasuw. 1 Grubość"]),
+                        shiftOfDriveRodBox1 = Convert.ToInt32(row["Puszka Zasuw. 1 Grubość"]),
+
+                        heightOfDriveRodBox2 = Convert.ToInt32(row["Puszka Zasuw. 2 Wysokość"]),
+                        depthOfDriveRodBox2 = Convert.ToInt32(row["Puszka Zasuw. 2 Głębokość"]),
+                        thicknessOfDriveRodBox2 = Convert.ToInt32(row["Puszka Zasuw. 2 Grubość"]),
+                        shiftOfDriveRodBox2 = Convert.ToInt32(row["Puszka Zasuw. 2 Grubość"]),
+
+                        heightOfDriveRodBox3 = Convert.ToInt32(row["Puszka Zasuw. 3 Wysokość"]),
+                        depthOfDriveRodBox3 = Convert.ToInt32(row["Puszka Zasuw. 3 Głębokość"]),
+                        thicknessOfDriveRodBox3 = Convert.ToInt32(row["Puszka Zasuw. 3 Grubość"]),
+                        shiftOfDriveRodBox3 = Convert.ToInt32(row["Puszka Zasuw. 3 Grubość"]),
+
+                        heightOfDriveRodBox4 = Convert.ToInt32(row["Puszka Zasuw. 4 Wysokość"]),
+                        depthOfDriveRodBox4 = Convert.ToInt32(row["Puszka Zasuw. 4 Głębokość"]),
+                        thicknessOfDriveRodBox4 = Convert.ToInt32(row["Puszka Zasuw. 4 Grubość"]),
+                        shiftOfDriveRodBox4 = Convert.ToInt32(row["Puszka Zasuw. 4 Grubość"]),
+
+                        heightOfDriveRodBox5 = Convert.ToInt32(row["Puszka Zasuw. 5 Wysokość"]),
+                        depthOfDriveRodBox5 = Convert.ToInt32(row["Puszka Zasuw. 5 Głębokość"]),
+                        thicknessOfDriveRodBox5 = Convert.ToInt32(row["Puszka Zasuw. 5 Grubość"]),
+                        shiftOfDriveRodBox5 = Convert.ToInt32(row["Puszka Zasuw. 5 Grubość"]),
+
+                        heightOfDriveRodBox6 = Convert.ToInt32(row["Puszka Zasuw. 6 Wysokość"]),
+                        depthOfDriveRodBox6 = Convert.ToInt32(row["Puszka Zasuw. 6 Głębokość"]),
+                        thicknessOfDriveRodBox6 = Convert.ToInt32(row["Puszka Zasuw. 6 Grubość"]),
+                        shiftOfDriveRodBox6 = Convert.ToInt32(row["Puszka Zasuw. 6 Grubość"])
+
+                    };
+
+                    parametersOfDriveRods.Add(parametersOfDriveRodsInfo);
                     
 
                 }
@@ -132,7 +174,7 @@ namespace PackageImport
                 command.Parameters.Add("@opisOpak3", SqlDbType.NVarChar);
                 command.Parameters.Add("@ok_cel", SqlDbType.Int);
 
-
+                Console.WriteLine(resManager.GetString("Loading" ) + " nhandlo");
                 foreach (var item in articlesNhandlo)
                 {
 
@@ -150,7 +192,7 @@ namespace PackageImport
                     command.Parameters["@opisOpak3"].Value = item.opisOpak3;
                     command.Parameters["@ok_cel"].Value = item.ok_cel;
 
-                    Console.WriteLine(resManager.GetString("Loading" ) + " nhandlo");
+                    
 
                     try
                     {
@@ -172,12 +214,121 @@ namespace PackageImport
 
             }
 
+        private void SaveDriveRodsParameters(string databaseConnectionString)
+        {
+
+            using (SqlConnection con = new SqlConnection(databaseConnectionString))
+            using (SqlCommand command = con.CreateCommand())
+            {
+                con.Open();
+                command.Parameters.Add("@RowCount", SqlDbType.Int).Direction = ParameterDirection.Output;
+                command.Parameters.Add("@NR_ART", SqlDbType.NVarChar);
+                command.Parameters.Add("@handleHeight", SqlDbType.Int);
+                command.Parameters.Add("@dornmass", SqlDbType.Int);
+                
+                command.Parameters.Add("@heightOfDriveRodBox1", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox1", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox1", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox1", SqlDbType.Int);
+
+                command.Parameters.Add("@heightOfDriveRodBox2", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox2", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox2", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox2", SqlDbType.Int);
+
+                command.Parameters.Add("@heightOfDriveRodBox3", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox3", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox3", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox3", SqlDbType.Int);
+
+                command.Parameters.Add("@heightOfDriveRodBox4", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox4", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox4", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox4", SqlDbType.Int);
+
+                command.Parameters.Add("@heightOfDriveRodBox5", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox5", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox5", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox5", SqlDbType.Int);
+
+                command.Parameters.Add("@heightOfDriveRodBox6", SqlDbType.Int);
+                command.Parameters.Add("@depthOfDriveRodBox6", SqlDbType.Int);
+                command.Parameters.Add("@thicknessOfDriveRodBox6", SqlDbType.Int);
+                command.Parameters.Add("@shiftOfDriveRodBox6", SqlDbType.Int);
+
+
+                Console.WriteLine(resManager.GetString("Loading") + " zasow");
+                foreach (var item in parametersOfDriveRods)
+                {
+
+                    command.CommandText = "UPDATE DBO.ZASOW SET wys_klam = @handleHeight, szerpuszki=@thicknessOfDriveRodBox1, glebpuszki = @depthOfDriveRodBox1,  ";
+                    command.CommandText += "polozenie2=@heightOfDriveRodBox2, szerpuszk2=@thicknessOfDriveRodBox2, glebpuszk2 = @depthOfDriveRodBox2, ";
+                    command.CommandText += "polozenie3=@heightOfDriveRodBox3, szerpuszk3=@thicknessOfDriveRodBox3, glebpuszk3 = @depthOfDriveRodBox3, ";
+                    command.CommandText += "polozenie4=@heightOfDriveRodBox4, szerpuszk4=@thicknessOfDriveRodBox4, glebpuszk4 = @depthOfDriveRodBox4, ";
+                    command.CommandText += "polozenie5=@heightOfDriveRodBox5, szerpuszk5=@thicknessOfDriveRodBox5, glebpuszk5 = @depthOfDriveRodBox5, ";
+                    command.CommandText += "polozenie6=@heightOfDriveRodBox6, szerpuszk6=@thicknessOfDriveRodBox6, glebpuszk6 = @depthOfDriveRodBox6 ";
+                    command.CommandText += " WHERE NR_ART=@NR_ART  SET @RowCount = @@ROWCOUNT;";
+
+                    command.Parameters["@nr_art"].Value = item.nr_art;
+                    command.Parameters["@handleHeight"].Value = item.handleHeight;
+                    command.Parameters["@dornmass"].Value = item.dornmass;
+                    
+                    command.Parameters["@heightOfDriveRodBox1"].Value = item.heightOfDriveRodBox1;
+                    command.Parameters["@depthOfDriveRodBox1"].Value = item.depthOfDriveRodBox1;
+                    command.Parameters["@thicknessOfDriveRodBox1"].Value = item.thicknessOfDriveRodBox1;
+                    command.Parameters["@shiftOfDriveRodBox1"].Value = item.shiftOfDriveRodBox1;
+
+                    command.Parameters["@heightOfDriveRodBox2"].Value = item.heightOfDriveRodBox2;
+                    command.Parameters["@depthOfDriveRodBox2"].Value = item.depthOfDriveRodBox2;
+                    command.Parameters["@thicknessOfDriveRodBox2"].Value = item.thicknessOfDriveRodBox2;
+                    command.Parameters["@shiftOfDriveRodBox2"].Value = item.shiftOfDriveRodBox2;
+
+                    command.Parameters["@heightOfDriveRodBox3"].Value = item.heightOfDriveRodBox3;
+                    command.Parameters["@depthOfDriveRodBox3"].Value = item.depthOfDriveRodBox3;
+                    command.Parameters["@thicknessOfDriveRodBox3"].Value = item.thicknessOfDriveRodBox3;
+                    command.Parameters["@shiftOfDriveRodBox3"].Value = item.shiftOfDriveRodBox3;
+
+                    command.Parameters["@heightOfDriveRodBox4"].Value = item.heightOfDriveRodBox4;
+                    command.Parameters["@depthOfDriveRodBox4"].Value = item.depthOfDriveRodBox4;
+                    command.Parameters["@thicknessOfDriveRodBox4"].Value = item.thicknessOfDriveRodBox4;
+                    command.Parameters["@shiftOfDriveRodBox4"].Value = item.shiftOfDriveRodBox4;
+
+                    command.Parameters["@heightOfDriveRodBox5"].Value = item.heightOfDriveRodBox5;
+                    command.Parameters["@depthOfDriveRodBox5"].Value = item.depthOfDriveRodBox5;
+                    command.Parameters["@thicknessOfDriveRodBox5"].Value = item.thicknessOfDriveRodBox5;
+                    command.Parameters["@shiftOfDriveRodBox5"].Value = item.shiftOfDriveRodBox5;
+
+                    command.Parameters["@heightOfDriveRodBox6"].Value = item.heightOfDriveRodBox6;
+                    command.Parameters["@depthOfDriveRodBox6"].Value = item.depthOfDriveRodBox6;
+                    command.Parameters["@thicknessOfDriveRodBox6"].Value = item.thicknessOfDriveRodBox6;
+                    command.Parameters["@shiftOfDriveRodBox6"].Value = item.shiftOfDriveRodBox6;
+
+                    //int RowsAffected = Convert.ToInt32(command.Parameters["@RowCount"].Value); dla sprawdzenia czy rekord został odnaleziony w bazie
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.ReadKey();
+                    }
+
+                }
+                con.Close();
+                Console.WriteLine("Press any key to end");
+                Console.ReadKey();
+            }
+        }
+
         public static void Main(string[] args)
 
         {
             Program program = new Program(@"C:\Users\tomasz.hoffmann\Desktop\LPT 17072019.xlsx", @"Data Source=.\sqlexpress;Initial Catalog=BMW5.6.2.1; Integrated Security=False;User ID=sa;Password=Whokna123@");
             program.LoadExcel(program._excelPath);
             program.SaveNhandloParameters(program._databaseConnectionString);
+            program.SaveDriveRodsParameters(program._databaseConnectionString);
         }
         
 
